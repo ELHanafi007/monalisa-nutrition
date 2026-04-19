766e66891e110014bebae1088b8289d8d0249b6f
mac <mac@MacBook-Pro-de-mac.local>
2026-04-19 12:35:21 +0100

    Add diagnostics page at /test

---
+++ src/app/test/page.tsx
@@ -0,0 +1,9 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: '100px', textAlign: 'center', backgroundColor: 'black', color: 'white', height: '100vh' }}>
+      <h1 style={{ fontSize: '50px' }}>SERVER IS ALIVE 🚀</h1>
+      <p>If you can see this, the infrastructure is working. The problem is in the main site components.</p>
+      <a href="/" style={{ color: 'red', textDecoration: 'underline' }}>Go back to home</a>
+    </div>
+  );
+}
